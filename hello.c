a85715d7c7ded532b88db776f758cc5d9a43d58d
Tylerengle158 <69804385+Tylerengle158@users.noreply.github.com>
2020-08-23 15:00:28 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tyler Engle
+ * Date: 2020/08/17
+ *
+ * A hello to the world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Tyler Engle!\n");
+
+     return 0;
+ }
